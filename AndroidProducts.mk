8f4635b9d43918ccc4d09a79a7ad0cacbdba2e7e
Lostark13 <tushar9915@gmail.com>
2020-10-16 16:29:17 +0200

    surya: Bringup RR

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_surya.mk
+    $(LOCAL_DIR)/rr_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_surya-user \
-    havoc_surya-userdebug \
-    havoc_surya-eng
+    rr_surya-user \
+    rr_surya-userdebug \
+    rr_surya-eng
--- rr_surya.mk
+++ rr_surya.mk
@@ -7,10 +7,14 @@
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+#RR-Stuff
+RR_BUILDTYPE := Official
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_surya
+PRODUCT_NAME := rr_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
@@ -19,5 +23,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.3.0.QJGMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-HAVOC_BUILD_TYPE := Official
