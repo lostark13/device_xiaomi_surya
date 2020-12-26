e9dac12e1d5664bbdd84637d2d7b32e86647932c
varund7726 <date.varun123@gmail.com>
2020-12-26 12:30:07 +0530

    surya: Build RR wallpapers

--- rr_surya.mk
+++ rr_surya.mk
@@ -12,6 +12,7 @@ $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 #RR-Stuff
 RR_BUILDTYPE := Official
 TARGET_FACE_UNLOCK_SUPPORTED := true
+BUILD_RR_WALLPAPERS:= true
 
 #Gapps
 $(call inherit-product-if-exists, vendor/gapps/config.mk)
