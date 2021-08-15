b8977463fa4100c5e4044098959c7724da554981
Lostark13 <tushar9915@gmail.com>
2021-08-15 20:00:45 +0200

    surya: Switch to OctaviOS Doze

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.haptic.HapticUtils;
 
@@ -36,7 +35,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize();
-        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
         HapticUtils.restoreLevel(context);
     }
