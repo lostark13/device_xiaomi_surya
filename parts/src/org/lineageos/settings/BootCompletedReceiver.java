6f468ba954c93a51bccf50ad10b8ca26b382c9f9
Lostark13 <tushar9915@gmail.com>
2021-10-22 07:04:44 +0000

    Revert "surya: Switch to OctaviOS Doze"
    This reverts commit b8977463fa4100c5e4044098959c7724da554981.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.haptic.HapticUtils;
 
@@ -35,6 +36,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize();
+        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
         HapticUtils.restoreLevel(context);
     }
