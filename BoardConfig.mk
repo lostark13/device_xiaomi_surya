0b6dd4b8e304f03c795c7ce5704f43e8574145c5
Lostark13 <tushar9915@gmail.com>
2021-10-31 12:19:10 +0530

    surya: Switch to enforcing mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,6 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Media
 TARGET_USES_ION := true
