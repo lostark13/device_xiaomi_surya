1fc80f52501d70c10ec80366bd1b96cc5054d251
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-12-24 15:40:20 +0530

    surya: Remove ANT+ HAL for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,9 +42,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
 
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := surya,karna
 
