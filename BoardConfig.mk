da3cd898ebecf64063ab0c8d512863dda1023ee2
Tushar Bharti <tushar9915@gmail.com>
2022-11-11 19:49:58 +0530

    surya: switch to legacy sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -205,7 +205,7 @@ VENDOR_SECURITY_PATCH := 2022-10-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
